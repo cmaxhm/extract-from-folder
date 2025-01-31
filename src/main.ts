ca9f8b6ae1f4a677540d712866ddd064b58bef80
Carlos Castillo <cmaxhm@yahoo.com>
2025-01-31 14:32:05 -0500

    Add CLI prompt

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import fs from 'fs';
+import * as readline from 'node:readline';
 
 const basePath: string = './content/';
 const outputPath: string = __dirname + '/.' + basePath;
+const readLine = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 function moveFiles(path: string) {
   fs.readdir(path, (err: NodeJS.ErrnoException | null, files: string[]) => {
@@ -23,4 +28,37 @@ function moveFiles(path: string) {
   });
 }
 
-moveFiles(basePath);
+
+function moveVideoFiles(path: string) {
+  fs.readdir(path, (err: NodeJS.ErrnoException | null, files: string[]) => {
+    files.forEach((element: string): void => {
+      const filePath: string = path + element;
+
+      fs.stat(filePath, (err1: NodeJS.ErrnoException | null, stats: fs.Stats): void => {
+        if (err1) throw err1;
+
+        if (stats.isDirectory()) {
+          moveVideoFiles(filePath + '/');
+        } else {
+          if (element.includes('.mp4')) {
+            fs.rename(filePath, outputPath + element, (err2: NodeJS.ErrnoException | null) => {
+              if (err2) throw err2;
+            });
+          }
+        }
+      });
+    });
+  });
+}
+
+readLine.question('Please select the type of files you want to move (1: All files, 2: Video files): ', (answer: string) => {
+  if (answer === '1') {
+    moveFiles(basePath);
+  } else if (answer === '2') {
+    moveVideoFiles(basePath);
+  } else {
+    console.log('Invalid input');
+  }
+
+  readLine.close();
+});
